d7a8f7f946ddf2f2b6d6fa41980564db56d8c159
filipe carnevale do nascimento <filipecn633@gmail.com>
2023-09-12 21:40:55 -0300

    LeNumerosAte0.java

--- 12_9/LeNumerosAte0.java
+++ 12_9/LeNumerosAte0.java
@@ -3,14 +3,26 @@ public class LeNumerosAte0 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("digite seus numeros, 0 encerra");
+        int cont = 0;
         int soma = 0;
         int n = scanner.nextInt();
         while (n != 0) {
             soma = soma + n;
+            cont = cont + 1;
             System.out.print("proximo: ");
             n = scanner.nextInt();
         }
         System.out.println("soma dos valores: " + soma);
+        if (cont == 0) {
+            System.out.println("nenhum valor foi digitado ");
+
+        }
+        else {
+            double media = (double)soma / cont;
+            System.out.println("media dos valores: " + media);
+        }
+        double media = (double)soma / cont;
+        System.out.println("media dos valores: " + media);
         scanner.close();
     }
 }
